bc2f51af22a7b7433a61fe3628b6333d3e7d3249
Adrian Schofield <adrianschofield@hotmail.co.uk>
2020-04-18 07:27:14 +0100

    Initial Commit. Cards are dealt to the tableau and that is all

---
+++ Assets/Scripts/Solitaire.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class Solitaire : MonoBehaviour
+{
+    public Sprite[] cardFaces;
+    public GameObject cardPrefab;
+    public GameObject[] bottomPos;
+    public GameObject[] topPos;
+    public static string[] suits = new string[] { "C", "D", "H", "S" };
+    public static string[] values = new string[] { "A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
+    public List<string>[] bottoms;
+    public List<string>[] tops;
+
+    private List<string> bottom0 = new List<string>();
+    private List<string> bottom1 = new List<string>();
+    private List<string> bottom2 = new List<string>();
+    private List<string> bottom3 = new List<string>();
+    private List<string> bottom4 = new List<string>();
+    private List<string> bottom5 = new List<string>();
+    private List<string> bottom6 = new List<string>();
+    private List<string> bottom7 = new List<string>();
+    private List<string> bottom8 = new List<string>();
+
+
+    public List<string> deck;
+    // Start is called before the first frame update
+    void Start()
+    {
+        bottoms = new List<string>[] {bottom0, bottom1, bottom2, bottom3, bottom4, bottom5, bottom6, bottom7, bottom8};
+        PlayCards();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PlayCards()
+    {
+        deck = GenerateDeck();
+        Shuffle(deck);
+
+        //test the cards in the deck
+        foreach(string card in deck)
+        {
+            print(card);
+        }
+        SolitaireSort();
+        StartCoroutine(SolitaireDeal());
+    }
+
+    public static List<string> GenerateDeck() 
+    {
+        List<string> newDeck = new List<string>();
+        foreach(string s in suits)
+        {
+            foreach(string v in values)
+            {
+                newDeck.Add(s + v);
+            }
+        }
+        return newDeck;
+    }
+
+    void Shuffle<T>(List<T> list){
+        System.Random random = new System.Random();
+        int n = list.Count;
+        while(n > 1)
+        {
+            int k = random.Next(n);
+            n--;
+            T temp = list[k];
+            list[k] = list[n];
+            list[n] = temp;
+        }
+
+    }
+
+    IEnumerator SolitaireDeal()
+    {
+        for(int i = 0; i < 9; i++)
+        {
+            float yOffset = 0;
+            float zOffset = 0.03f;
+            foreach(string card in bottoms[i])
+            {
+                yield return new WaitForSeconds(0.01f);
+                GameObject newCard = Instantiate(cardPrefab, new Vector3(bottomPos[i].transform.position.x, bottomPos[i].transform.position.y - yOffset, bottomPos[i].transform.position.z - zOffset), Quaternion.identity, bottomPos[i].transform);
+                newCard.name = card;
+                newCard.GetComponent<Selectable>().faceUp = true;
+
+                yOffset = yOffset + 0.3f;
+                zOffset = zOffset + 0.03f;
+            }
+        }
+    }
+
+    void SolitaireSort()
+    {
+        for (int i = 0; i < 9; i++)
+        {
+            for (int j = i; j < 9; j++)
+            {
+                bottoms[j].Add(deck.Last<string>());
+                deck.RemoveAt(deck.Count - 1);
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # unshaped-KingAlbertSolitaire
 Unity version of the solitaire game King Albert
+
+# Sprite Art Attribution
+
+https://opengameart.org/content/playing-cards-0
+
+"Terms & Conditions: The free game assets on this site are released under the Creative Commons Attribution Licence. This basically means that you can download them, use them in your game(s) and modify them as you like. If you use them in your games, we’d appreciate a small mention somewhere in the credits / on the website (and a free copy of the game would be cool!). Basically, the only thing we don’t want you to do with our assets is sell them as assets to other developers, either individually, or as part of a collection."
+
+# Tutorial Attribution
+
+Solely based on this awesome tutorial series from Megalomobile
+
+https://www.youtube.com/playlist?list=PLuq_iMEtykn8XiZb6453f61U7HC7u8KiC
